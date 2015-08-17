aa94463dae43160bb8cc59b62ba312dbca2b56ec
Guest User <student@epicodus.com>
2015-08-17 14:48:41 -0700

    pass isosceles test

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -26,7 +26,9 @@ public class Triangle{
     if((mSideA == mSideB) && (mSideC == mSideB)){
       result = "equilateral";
     }
-//    else if(())
+   else if((mSideA == mSideB) || (mSideB == mSideC)){
+     result = "isosceles";
+   }
     return result;
   }
 }
