373739b484f65e4919c0c98fe4b5a74e3e0362e9
Guest User <student@epicodus.com>
2015-08-17 13:57:14 -0700

    Instanciates correctly

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -3,7 +3,8 @@ import static org.junit.Assert.*;
 
 public class TriangleTest {
 
-  @Test void newTriangle_instanciatesCorrectly(){
+  @Test
+  public void newTriangle_instanciatesCorrectly(){
     Triangle testTriangle = new Triangle (3, 3, 3);
     assertEquals(true, testTriangle instanceof Triangle);
   }
