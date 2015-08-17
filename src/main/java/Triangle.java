49793d75059964ee86b005aa150ccd2e85b824c8
Guest User <student@epicodus.com>
2015-08-17 14:24:04 -0700

    tests triangles

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,14 +1,23 @@
+import java.util.Arrays;
+
 public class Triangle{
   private int mSideA;
   private int mSideB;
   private int mSideC;
 
   public Triangle(int sideA, int sideB, int sideC) {
-    mSideA = sideA;
-    mSideB = sideB;
-    mSideC = sideC;
-  }
+    int sidesArray[] = {sideA, sideB, sideC};
+    Arrays.sort(sidesArray);
+    mSideA = sidesArray[0];
+    mSideB = sidesArray[1];
+    mSideC = sidesArray[2];
+    }
+
   public boolean isTriangle(){
-    return true;
+    if ((mSideA + mSideB) > mSideC) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
